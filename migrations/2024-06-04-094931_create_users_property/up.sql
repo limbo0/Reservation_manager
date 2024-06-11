5d621bf6a97a7e81fcf3f695495b30486b481a8f
khewa <97kidkhewa@gmail.com>
2024-06-11 15:19:09 +0530

    Removed unnecessary directories
    migrations, migrations_backup, plan.md

--- migrations/2024-06-04-094931_create_users_property/up.sql
+++
@@ -1,9 +0,0 @@
--- Your SQL goes here
-
-CREATE TABLE propertyusers (
-  user_id SERIAL NOT NULL PRIMARY KEY,
-  user_name VARCHAR NOT NULL,
-  user_password VARCHAR NOT NULL,
-  user_role INTEGER NOT NULL REFERENCES roles(role_id),
-  property_id uuid NOT NULL REFERENCES property(property_id)
-);
